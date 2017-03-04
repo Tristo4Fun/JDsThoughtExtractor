03fffdf3cfb641685e65c4acb7302c766d451f7a
Tristan Vujovic <tristan.vujovic@gmail.com>
2017-03-04 15:18:48 +0100

    initial commit

---
+++ Scrubs Subtitles/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Scrubs_Subtitles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.  richTextBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(645, 388);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(181, 93);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            //   richTextBox1
+            // 
+            this.  richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.  richTextBox1.Location = new System.Drawing.Point(291, 12);
+            this.  richTextBox1.Multiline = true;
+            this.  richTextBox1.Name = "  richTextBox1";
+            this.  richTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.  richTextBox1.Size = new System.Drawing.Size(286, 469);
+            this.  richTextBox1.TabIndex = 1;
+            this.  richTextBox1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(37, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "[Energize]";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.LightSeaGreen;
+            this.linkLabel1.Location = new System.Drawing.Point(28, 461);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(115, 28);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "[My Twitter]";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(615, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 92);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.Location = new System.Drawing.Point(22, 356);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(228, 97);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(50, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "[RANDOM JD THOUGHT]";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(149, 458);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 31);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "[GENERATE]";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(149, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(41, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(149, 111);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(41, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(30, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 37);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Season:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(28, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 37);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Episode:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(612, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "[Subtitle Format]";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(660, 161);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 64);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "[Find in subtitles]";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(598, 69);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(215, 81);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ye);
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(838, 493);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.  richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "J.D.s thought extracter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox   richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Scrubs Subtitles/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrubs_Subtitles
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int episodemax = 0;
+            string season = textBox4.Text;
+
+            string exePath = Application.StartupPath;
+            
+                if (textBox4.Text.Length == 0 || int.Parse(textBox4.Text) < 0 || int.Parse(textBox4.Text) > 8)
+                richTextBox1.Text = "False input: Season";
+            else
+            {
+                if (int.Parse(season) == 1) episodemax = 24;
+                if (int.Parse(season) == 2) episodemax = 22;
+                if (int.Parse(season) == 3) episodemax = 22;
+                if (int.Parse(season) == 4) episodemax = 25;
+                if (int.Parse(season) == 5) episodemax = 24;
+                if (int.Parse(season) == 6) episodemax = 21;
+                if (int.Parse(season) == 7) episodemax = 11;
+                if (int.Parse(season) == 8) episodemax = 18;
+                   
+                if (textBox5.Text.Length == 0 || int.Parse(textBox5.Text) > episodemax || int.Parse(textBox5.Text)<0)
+                      richTextBox1.Text = "False input: Episode";
+                else
+                {
+                    string episode = exePath + "\\Resources\\Season" + season + "\\" + textBox5.Text + ".txt";
+
+                    StreamReader objstream = new StreamReader(episode); //Treba mi je using System.IO;
+
+                    textBox1.Text = objstream.ReadToEnd();
+
+                      richTextBox1.Clear();  /* Znaci,prvo sam uzea subtitle format i pretvoria ga u obicna slova ali
+                                           sam misli stavia u " ".Onda sam procistia taj drugi format tako sta sam
+                                          prizna samo ono u " " kao prolazno i usput zaminia " sa ' 'i trim() */
+                    string lol = "";
+                    string scrub = "";
+                    string scrub2 = "";
+                    int br = 0;
+                    bool zad = false, isitnumber = false;
+                    char[] numbandsign = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',', ':', ';', '-' };
+                    lol = textBox1.Text.Trim();
+
+                    foreach (char b in lol)
+                    {
+                        if (b == '>' || zad == true)
+                        {
+
+                            if (numbandsign.Contains<char>(b) == true)
+                                isitnumber = false;
+                            if (b != '>' && b != '<' && numbandsign.Contains<char>(b) == false)
+                             scrub += b;
+                            zad = true;
+                        
+                        }
+                        if (b == '<')
+                        {
+
+                            scrub += '"';
+                            zad = false;
+                        }
+                    }
+
+                    zad = true;
+                    foreach (char b in scrub)
+                    {
+                        if (b == '"' || zad == true)
+                        {
+
+                            if (br == 1 && b == '"')
+                            {
+                                br = 0;
+                                zad = false;
+                            }
+                            if (b == '"' && zad == true) br = 1;
+
+                            if (br == 1)                   //Ako petlja prolazi recenicama kojima zelimo da prolazi
+                                scrub2 += b;
+                            zad = true;
+                        }
+
+                    }
+
+
+                    string scrub3 = scrub2.Replace('"', ' ');
+                    scrub3 = scrub3.Trim(' ');
+                    if (scrub3.Length > 32)
+                        scrub3 = scrub3.Remove(scrub3.Length - 32);
+                    else
+                          richTextBox1.Text = "False input lenght";  
+                      richTextBox1.Text = " " + scrub3;
+
+                }
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.twitter.com/TristanVujovic");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (  richTextBox1.Text == "")
+            {
+                textBox3.Font = new Font(textBox3.Font, FontStyle.Italic);
+                textBox3.Text = "Nothing to quote";
+            }
+            else
+            {
+                textBox3.Font = new Font(textBox3.Font, FontStyle.Regular);
+                Random p = new Random();
+
+                int rec = p.Next(0,   richTextBox1.Text.Length);
+                int br = -1, lol = 0;
+                string quote = "";
+                foreach (char b in   richTextBox1.Text)
+                {
+                    br++;
+                    if (br >= rec)
+                    {
+                        if (char.IsUpper(b))                       //Od Random tocke u   richTextBox1.Textu,kad detektira veliko slovo pocni recenicu i spremaj je u quote
+                            lol = 1;
+                        if (lol == 1)
+                            quote += b;
+                    }
+                    if (b == '.' && lol == 1)  //Ako je petlja iznad zeljene recenice i naide na tocku,izadi
+                        break;
+                }
+                textBox3.Text = quote;
+
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            string felop = richTextBox1.Text;
+            richTextBox1.Text = "";
+            richTextBox1.Text = felop;
+
+            while(index < richTextBox1.Text.LastIndexOf(textBox3.Text))
+            {
+                richTextBox1.Find(textBox3.Text.LastIndexOf(textBox3.Text))
+
+            }
+
+
+
+            /*
+            string[] felop = new string[400];
+            string box2 = "";
+            if (  richTextBox1.Text == "")
+            {
+                textBox6.Font = new Font(textBox6.Font, FontStyle.Italic);
+                textBox6.Text = "Nothing to search for";
+            }
+            else
+            {
+
+                felop = textBox6.Text.Split(' ','.');
+                box2 =   richTextBox1.Text.ToLower();
+                foreach (string p in felop)
+                {
+                    if (box2.Contains(p))
+                    {
+                        textBox1.Text += " " + p;
+
+                    }
+                }
+
+
+
+            }
+ 
+              int  br = 0, br1 = 0,br2 = 0;
+              string[] trica = new string[30];
+              string[] duja = new string[500];
+              duja =   richTextBox1.Text.Split(' ');
+              trica = textBox3.Text.Split(' ');
+              if (textBox3.Text != "" &&   richTextBox1.Text != "")
+              {
+                  trica = textBox3.Text.Split(' ');
+                  foreach (string k in trica)
+                  {
+                      br2 ++;                       //Broj stringova u textbox3 tj duljina
+                      foreach (string p in duja)
+                      {
+                          br1++;                                 //Broj stringova u textbox2  
+                      }
+
+
+                      if (  richTextBox1.Text.Contains(k))
+                      {
+                          br++;                                                //Broj stringova od pocetka txt3 do kraja txt 2 (txt3-txt2 = pocetak u txt 2)
+
+                      }
+
+                  }
+                    richTextBox1.SelectionStart = 0;
+                    richTextBox1.SelectionLength = br2-br1;
+
+
+              }
+        */
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+           public void ye (object sender,EventArgs e)
+            {
+            textBox6.Text = "";
+        }
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+      
+               
+        }
+
+        private void ye(object sender, MouseEventArgs e)
+        {
+            if (textBox6.Text == "Nothing to search for")
+            textBox6.Text = "";
+        }
+    }
+
+}
